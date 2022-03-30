e8db3a473f26baed99ba73b502462b69b779e0f3
hooting76 <60126117+hooting76@users.noreply.github.com>
2022-03-30 18:15:48 +0900

    로딩화면 수정중

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,6 +1,30 @@
-let header_el = $('#header .header_inner li');
+// 로딩화면
 
-$(window).onload(function(){
-    header_el.eq(0).css({left:0});
-    header_el.eq(1).css({right:0});
+$('.progress_bar').each(function () {
+    let progressWrap = $(this),
+        progressBar = progressWrap.find('.bar'),
+        progressText = progressWrap.find('.num'),
+        progressNum = progressText.attr('data-num');
+
+    progressBar.animate({ width: progressNum + "%" }, 2000);
+
+    setInterval(textAni, 100);
+    function textAni() {
+        let currentWidth = progressBar.width() / progressWrap.width() * 100;
+        progressText.text(Math.ceil(currentWidth) + "%");
+
+        // console.log(currentWidth);
+        if(currentWidth==100){
+            $('.progress_bar').css('transform','scale(1.4)');
+            $('.progress_bar').css('transform','scale(0)');
+        }
+    };
 });
+
+// 해더 fadein
+let header_el = $('#header ul li');
+
+window.onload = function(){
+    header_el.animate({opacity:1},1000);
+};
+
