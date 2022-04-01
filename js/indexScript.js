58b97d98ebdbf8e486536c0990c5a65185ac6873
hooting76 <60126117+hooting76@users.noreply.github.com>
2022-04-01 18:15:04 +0900

    로딩화면 폰트변경, 일부아이콘 추가, 미니메뉴 단추 완료

--- js/indexScript.js
+++ js/indexScript.js
@@ -17,15 +17,42 @@ $('.progress_bar').each(function () {
             progressText.text('완료!')
             setTimeout(function(){
                 $('.loading').css('transform','scale(0)');
-            },800)
+            },800)          
         }
     };
 });
 
-// 해더 fadein
+// 해더 fadein효과
 let header_el = $('#header ul li');
 
-window.onload = function(){
-    header_el.animate({opacity:1},3000);
-};
+setTimeout(function(){
+    header_el.animate({opacity:1},1000);
+},3000)
+
+// 더보기 plus버튼
+let plusBtn = $('#wrap #main .main_inner .main_text .mini_menu .plus');
+let line1 = plusBtn.find('.line1');
+let line2 = plusBtn.find('.line2');
+
+// console.log(menuWidth);
+plusBtn.click(function(){
+    // console.log(menuWidth);
+    if($('.mini_menu').css('width') =="40px"){
+        line1.css('transform','rotate(45deg)');
+        line2.css('transform','rotate(135deg)');
+        $('.mini_menu').css('width','150px');
+    }
+    else if($('.mini_menu').css('width') == "150px"){
+        line1.css('transform','rotate(90deg)');
+        line2.css('transform','rotate(0)');
+        $('.mini_menu').css('width','40px');
+    }
+})
+
+// 배너 텍스트 효과
+let bannerText = $('.main_text h1');
+setTimeout(function(){
+    bannerText.css('margin-right','0').css('opacity','1');
+},3000);
+
 
