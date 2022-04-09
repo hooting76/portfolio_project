91ba5a182746834c8460075a482ae81046bc4860
hooting76 <ggongco1120@gmail.com>
2022-04-09 16:09:00 +0900

    배너작업중(map api확인용 푸시)

--- js/indexScript.js
+++ js/indexScript.js
@@ -66,3 +66,64 @@ let bulb = $('.fa-lightbulb');
 setTimeout(function(){
     bulb.css('color',"#e3dc4e");
 },5000);
+
+
+// about메뉴 관련
+let leftBtn = $('.arr_area>a>.fa-angle-left');
+let rightBtn = $('.arr_area>a>.fa-angle-right');
+
+leftBtn.click(function(){
+    let prevIdx = $('.about_photo img.active').index();
+    let rightText = $('.about_right_text');
+
+    // console.log(prevIdx);
+    $('.about_photo img').removeClass('active');
+    rightText.removeClass('active');
+
+    let nextIdx = prevIdx-1;
+    if(nextIdx== -1) nextIdx=1;
+    // console.log(nextIdx);
+
+    $('.about_photo img').eq(nextIdx).addClass('active');
+    rightText.eq(nextIdx).addClass('active');
+});
+
+
+rightBtn.click(function(){
+    let prevIdx = $('.about_photo img.active').index();
+    let rightText = $('.about_right_text');
+
+    // console.log(prevIdx);
+    $('.about_photo img').removeClass('active');
+    rightText.removeClass('active');
+
+    let nextIdx = prevIdx+1;
+    if(nextIdx== 2) nextIdx=0;
+    // console.log(nextIdx);
+
+    $('.about_photo img').eq(nextIdx).addClass('active');
+    rightText.eq(nextIdx).addClass('active');
+});
+
+// 지도api
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(37.16351442943923, 127.06035386912332), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 마커가 표시될 위치입니다 
+var markerPosition = new kakao.maps.LatLng(37.16351442943923, 127.06035386912332); 
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+// 마커가 드래그 가능하도록 설정합니다 
+marker.setDraggable(false); 
