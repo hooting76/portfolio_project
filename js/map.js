3574168020b8545e0f1cb46da99dc20dfffa47d6
hooting76 <ggongco1120@gmail.com>
2022-04-10 21:32:11 +0900

    맵 api 수정중

--- js/map.js
+++ js/map.js
@@ -3,7 +3,7 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
         center: new kakao.maps.LatLng(37.16351442943923, 127.06035386912332), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
+        level: 5 // 지도의 확대 레벨
     };
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
